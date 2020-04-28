ccc4535636dcb443f1a9062379cbd216f9878e23
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2020-04-28 23:29:12 +0200

    Text scripts for the help tabs

---
+++ shiney_app/Abo_uitleg_script.R
@@ -0,0 +1,14 @@
+p(h2(em("Abo 1")),
+  h4("NS.nl tekst"),
+  br(),
+  br(),
+  br(),
+  
+  
+  
+  h2(em("Abo 2")),
+  h4("NS.nl tekst"),
+  br(),
+  br(),
+  br()
+)
---
+++ shiney_app/Help_info_script.R
@@ -0,0 +1,31 @@
+
+p(h2(em("Princiepe van de NS-abo kiezer")),
+  h4("je moet gewoon ff chillen ok"),
+  br(),
+  br(),
+  br(),
+  
+  
+  
+  h2(em("Gegevens invoeren")),
+  h4("Vul in hoeveel dagen van de maand je een bepaalde treinreis wilt maken"),
+  h4("Hoeveel ritten zijn daarvan buiten de spits?"),
+  br(),
+  br(),
+  br(),
+  
+  h2(em("Uitkomst van de berekening")),
+  h4("Aan de hand van de ingevulde gegevens worden de maandelijkse kosten per NS abonnement berekend."),
+  br(),
+  h4("De voordeligste optie wordt bovenaan de pagina, in dikgedrukte letters, gegeven"),
+  br(),
+  h4("De kostenopbouw van elke NS-abonnementsvorm wordt weergegeven in de grafiek"),
+  h5("NB: Het", em("Altijd Vrij"), "abonnement (351 euro/maand) wordt alleen weergegeven wanneer dit een relevante optie kan zijn."),
+  br(),
+  h4("Onderaan de pagina staat een tabel met de maandelijkse kosten per NS-abonnement (gerangschikt op prijs).")
+  
+  
+  
+  
+  )
+
