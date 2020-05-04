e3954780cc9d067e62704b4b5787dfdd73494ebd
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2020-05-04 22:30:17 +0200

    Eddited help_info

--- shiney_app/Help_info_script.R
+++ shiney_app/Help_info_script.R
@@ -1,20 +1,16 @@
 span(
-p(h2(em("Princiepe van de NS-abo calculator")),
+p(h2(em("Principe van de NS-abo calculator")),
   h4("* De calculator berekent de maandelijkste kosten per beschikbaar NS abonnement.
      De kosten komen tot stand door een combinatie van: 
      aantal reisdagen / reistijden / ritkosten / kortingstarieven / abonnementskosten"),
   h4("* De calculator gaat er vanuit dat al je ritten dezelfde zijn! Je reist bijvoorbeeld een aantal dagen per maand naar je werk en naar huis"),
  
-  h2(em("Gegevens invoeren")),
-  h4("* Vul in hoeveel dagen van de maand je een bepaalde treinreis wilt maken"),
-  h4("* Hoeveel ritten zijn daarvan buiten de spits?"),
-
   h2(em("Uitkomst van de berekening")),
   h4("* Aan de hand van de ingevulde gegevens worden de maandelijkse kosten per NS abonnement berekend."),
   h4("* De voordeligste optie wordt bovenaan de pagina, in dikgedrukte letters, gegeven"),
   h4("* De kostenopbouw van elke NS-abonnementsvorm wordt weergegeven in de grafiek"),
   h5("NB: Het", em("Altijd Vrij"), "abonnement (351 euro/maand) wordt alleen weergegeven wanneer dit een relevante optie kan zijn."),
-  h4("* Onderaan de pagina staat een tabel met de maandelijkse kosten per NS-abonnement (gerangschikt op prijs).")
+  h4("* Naast de grafiek staat een tabel met de maandelijkse kosten per NS-abonnement (gerangschikt op prijs).")
   
   ),style="color:#405843"
 )
