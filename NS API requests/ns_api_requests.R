894d6a7475f69d513d7f38e56ea6be119d5139ec
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2020-05-25 00:03:58 +0200

    Create script for API request from NS.nl

---
+++ NS API requests/ns_api_requests.R
@@ -0,0 +1,35 @@
+library(httr)
+
+###############################
+# Input data for http request #
+###############################
+
+# Url to retrive data from
+ns.url = as.character("https://gateway.apiportal.ns.nl/public-prijsinformatie/prices?fromStation=Zaandam&toStation=Alkmaar")
+
+# Ocp-Apim-Subscription-Key -> should be in header pirmaire sleutel
+sub.key = as.character("92fd805f312b4907840fa436a2af87df")
+
+# Host: gateway.apiportal.ns.nl
+host = as.character("gateway.apiportal.ns.nl")
+
+################
+# HTTP Resuest #
+################
+
+# Create a header to send with the request
+ns.header = add_headers(.headers = 
+                          c(Host = host,
+                            "Ocp-Apim-Subscription-Key" = sub.key))
+
+request.result = content( # conent() is a JSON parser - it will create a list() object
+  GET(ns.url,
+    ns.header)
+)
+
+##########################################################
+# Get data you want (price single fare without discount) #
+##########################################################
+
+single.fare = (request.result$priceOptions[[2]]$totalPrices[[1]]$price)/100
+print(single.fare)
